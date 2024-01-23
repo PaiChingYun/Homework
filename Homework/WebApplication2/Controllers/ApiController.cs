f809897d702c91ceac9d136b674fc197ae698756
haku <bella20420422@gmail.com>
2024-01-23 09:16:11 +0800

    homework

---
+++ Homework/WebApplication2/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly MyDBContext _dbContext;
+
+        public ApiController(MyDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+            var cities= _dbContext.Addresses.Select(a => a.City).Distinct();
+            return Json(cities);
+        }
+    }
+}
